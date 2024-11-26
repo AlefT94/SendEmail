8ca5a7fd8156d39e9e69e648b9a84e73c21044a6
AlefT94 <alef_teodoro1@hotmail.com>
2024-11-25 21:21:19 -0300

    Adicionado envio de email

--- SendEmail/Program.cs
+++ SendEmail/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Asn1.Ocsp;
+using SendEmail.Services.Email;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var emailSettings = builder.Configuration.GetSection("EmailSettings").Get<EmailSettings>();
+builder.Services.AddSingleton(emailSettings);
+builder.Services.AddTransient<EmailService>();
+
 
 var app = builder.Build();
 
@@ -36,9 +45,28 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+// Endpoint para envio de e-mails
+app.MapPost("/send-email", async (EmailRequest request, EmailService emailService) =>
+{
+    try
+    {
+        await emailService.SendEmailAsync(request.ToEmail, request.Subject, request.Body);
+        return Results.Ok("E-mail enviado com sucesso!");
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem($"Erro ao enviar o e-mail: {ex.Message}");
+    }
+})
+.WithName("SendEmail")
+.WithOpenApi(); // Adiciona ao Swagger automaticamente (caso habilitado)
+
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+// Classe para o corpo da requisição
+public record EmailRequest(string ToEmail, string Subject, string Body);
---
+++ SendEmail/Services/Email/EmailService.cs
@@ -0,0 +1,33 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace SendEmail.Services.Email;
+
+public class EmailService
+{
+    private readonly EmailSettings _emailSettings;
+
+    public EmailService(EmailSettings emailSettings)
+    {
+        _emailSettings = emailSettings;
+    }
+
+    public async Task SendEmailAsync(string toEmail, string subject, string body)
+    {
+        var email = new MimeMessage();
+        email.From.Add(new MailboxAddress(_emailSettings.SenderName, _emailSettings.SenderEmail));
+        email.To.Add(new MailboxAddress("",toEmail));
+        email.Subject = subject;
+
+        email.Body = new TextPart("html")
+        {
+            Text = body
+        };
+
+        using var smtp = new SmtpClient();
+        await smtp.ConnectAsync(_emailSettings.SmtpServer, _emailSettings.Port, MailKit.Security.SecureSocketOptions.StartTls);
+        await smtp.AuthenticateAsync(_emailSettings.Username, _emailSettings.Password);
+        await smtp.SendAsync(email);
+        await smtp.DisconnectAsync(true);
+    }
+}
